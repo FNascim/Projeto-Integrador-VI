bde08e51ebadadcf91aa1d5ade30f9795fdcdd53
wsfer <93054415+wsfer@users.noreply.github.com>
2025-11-05 11:46:53 -0300

    Criação de função para obter log de eventos do servidor

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import type { Config } from "../types/config";
+import type { EventLog } from "../types/eventLog";
 
 const API_BASE = "http://127.0.0.1:5000";
 
@@ -11,3 +12,9 @@ export const getConfig = async (): Promise<Config> => {
 export const updateConfig = async (data: Config): Promise<void> => {
   await axios.post(`${API_BASE}/config`, data);
 };
+
+export const getEvents = async (): Promise<EventLog[]> => {
+  const res = await axios.get<EventLog[]>(`${API_BASE}/events`);
+  return res.data;
+};
+
