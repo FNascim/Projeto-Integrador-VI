38b68abe9c2c1fd9a849330a412c0401e42a9a4d
wsfer <93054415+wsfer@users.noreply.github.com>
2025-11-06 11:15:30 -0300

    Criado template para o componente EventCharts

--- frontend/src/components/EventCharts.tsx
+++ frontend/src/components/EventCharts.tsx
@@ -1,6 +1,41 @@
+import { useState } from "react";
+import { useEventLog } from "../hooks/useEventLog";
 import "./EventCharts.css";
 
+type ChartInterval = "week" | "month" | "year";
+
 export default function EventsChart() {
-  return <section className="event-charts">Em construção...</section>;
+  const { eventList, loading, error } = useEventLog();
+  const [chartInterval, setChartInterval] = useState<ChartInterval>("week");
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const newCategory = e.target.value as ChartInterval;
+    setChartInterval(newCategory);
+  };
+
+  return (
+    <section className="event-charts">
+      <h2>Tempo de uso</h2>
+
+      {error && <p className="error-message">{error}</p>}
+
+      <form method="post">
+        <select
+          className="chart-form-select"
+          name="interval"
+          value={chartInterval}
+          onChange={handleChange}
+        >
+          <option value="week">Semana atual</option>
+          <option value="month">Mês atual</option>
+          <option value="year">Ano atual</option>
+        </select>
+      </form>
+
+      <p>TODO tipo de dado: {chartInterval}</p>
+    </section>
+  );
 }
 
