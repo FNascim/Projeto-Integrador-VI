10faea15d881428b9816f106ac958e41f9acd019
wsfer <93054415+wsfer@users.noreply.github.com>
2025-11-07 15:30:15 -0300

    Mudança no algoritmo de cálculo do tempo de uso para lidar com eventos repetidos

--- frontend/src/utils/chartHelpers.ts
+++ frontend/src/utils/chartHelpers.ts
@@ -5,12 +5,13 @@ import type { EventLog } from "../types/eventLog";
  * Para o cálculo do tempo de uso é necessário que:
  * 1. O primeiro evento do dia deve ser do tipo 'ligada'
  * 2. O último evento do dia deve ser do tipo 'desligada'
- * 3. O número de eventos ligada e desligada deve ser o mesmo
+ * 3. O número de eventos ligada e desligada usados no cálculo deve ser o mesmo
+ *    3.1. Eventos repetidos sem um par diretamente em sequência são ignorados
  *
  * Funcionamento:
  *   - eventos do tipo 'ligada' tem timestamp negativo
  *   - eventos do tipo 'desligada' tem timestamp positivo
- *   - diferença = 'desligada' + 'ligada'
+ *   - calcula-se a diferença quando 'ligada' e 'desligada' vem em sequência
  *   - a soma das diferenças é o tempo de uso no dia
  */
 
@@ -65,31 +66,46 @@ interface UsageByDay {
   y: number;
 }
 
+interface GroupOfEvents {
+  [key: string]: EventLog[];
+}
+
 export function getUsageByDay(eventList: EventLog[]): UsageByDay[] {
   // Agrupa os eventos por dia
   // @ts-expect-error esse método é recente e o typescript gera alguns erros
-  const groupedEventsByDay = Object.groupBy(eventList, ({ timestamp }) =>
-    formatISO(timestamp, { representation: "date" })
+  const groupedEventsByDay: GroupOfEvents = Object.groupBy(
+    eventList,
+    ({ timestamp }: { timestamp: string }) =>
+      formatISO(timestamp, { representation: "date" })
   );
 
   // Ajusta os dados
   Object.values(groupedEventsByDay).forEach(eventArray => {
-    handleEventsBetweenDays(eventArray as EventLog[]);
+    handleEventsBetweenDays(eventArray);
   });
 
   // Cria os objetos do tipo { x: 'date', y: 'valor' }
   const result = Object.entries(groupedEventsByDay).map(([key, value]) => {
-    const totalTime = (value as EventLog[]).reduce(
-      (acc, { timestamp, event }) => {
-        const timeInMs = new Date(timestamp).getTime();
-        if (event === "ligada_auto" || event === "ligada_manual") {
-          return acc - timeInMs;
-        } else {
-          return acc + timeInMs;
-        }
-      },
-      0
-    );
+    let totalTime: number = 0;
+
+    // Calcula o tempo total que a lâmpada ficou ligada
+    for (let i = 0; i < value.length; i++) {
+      const currentEvent = value[i];
+      const nextEvent = value[i + 1];
+
+      if (
+        (currentEvent.event === "ligada_auto" ||
+          currentEvent.event === "ligada_manual") &&
+        nextEvent &&
+        (nextEvent.event === "desligada_auto" ||
+          nextEvent.event === "desligada_manual")
+      ) {
+        totalTime =
+          totalTime +
+          new Date(nextEvent.timestamp).getTime() -
+          new Date(currentEvent.timestamp).getTime();
+      }
+    }
 
     return { x: key, y: totalTime / 1000 / 60 / 60 };
   });
